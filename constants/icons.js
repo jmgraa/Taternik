1eb79ce79a474d6d29ab5f2400fb19b8a70b757f
Jakub Magiera <jakubmagiera.it@gmail.com>
2024-12-16 22:46:51 +0100

    assets upload

---
+++ constants/icons.js
@@ -0,0 +1,9 @@
+import profile from "../assets/icons/profile.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+
+export default {
+  profile,
+  eyeHide,
+  eye,
+};
---
+++ constants/images.js
@@ -0,0 +1,4 @@
+import logo from "../assets/images/logo.png";
+import tatrasWelcome from "../assets/images/tatras_welcome.jpg";
+
+export default { logo, tatrasWelcome };
