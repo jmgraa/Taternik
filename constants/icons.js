0bec33c6cdc67127c653b27bb779426d2b1c72d5
Jakub Magiera <jakubmagiera.it@gmail.com>
2024-12-30 16:37:48 +0100

    assets update

--- constants/icons.js
+++ constants/icons.js
@@ -1,9 +1,19 @@
 import profile from "../assets/icons/profile.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
+import search from "../assets/icons/search.png";
+import route from "../assets/icons/route.png";
+import locate from "../assets/icons/locate.png";
+import settings from "../assets/icons/settings.png";
+import map from "../assets/icons/map.png";
 
 export default {
   profile,
   eyeHide,
   eye,
+  search,
+  route,
+  locate,
+  settings,
+  map
 };
--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,17 @@
 import logo from "../assets/images/logo.png";
 import tatrasWelcome from "../assets/images/tatras_welcome.jpg";
+import tatra1 from "../assets/images/start_screen/tatra_1.jpg";
+import tatra2 from "../assets/images/start_screen/tatra_2.jpg";
+import tatra3 from "../assets/images/start_screen/tatra_3.jpg";
+import tatra4 from "../assets/images/start_screen/tatra_4.jpg";
+import tatra5 from "../assets/images/start_screen/tatra_5.jpg";
 
-export default { logo, tatrasWelcome };
+export default { 
+    logo, 
+    tatrasWelcome,
+    tatra1,
+    tatra2,
+    tatra3,
+    tatra4,
+    tatra5
+};
