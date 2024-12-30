117a376c1dd5614554bf2d871a2ce08ae93931fe
Jakub Magiera <jakubmagiera.it@gmail.com>
2024-12-30 16:37:28 +0100

    geojson type declaration

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+config.resolver.assetExts = [...config.resolver.assetExts, 'geojson'];
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'geojson'];
+
 module.exports = withNativeWind(config, { input: './constants/global.css' })
