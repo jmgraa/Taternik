871e6e0fe37403b1dec58a73e9a2a76f75179454
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-01-04 16:51:01 +0100

    add 'in implementation' info

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { ImageBackground, View, Text, Image } from 'react-native'
+import { images, icons } from '@/constants'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
+     <ImageBackground
+      source={images.tatra4}
+      resizeMode="cover"
+      className='flex-1'
+    >
+      <View className='flex-1 justify-center items-center bg-black/50'>
+        <Text className='text-3xl text-white text-semibold font-psemibold text-center'>Tab available soon</Text>
+        <Image
+          source={icons.settings}
+          resizeMode='contain'
+          className='w-[200px] h-[200px]'
+        />
+      </View>
+    </ImageBackground>
+  );
 }
 
 export default Profile
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { ImageBackground, View, Text, Image } from 'react-native'
+import { images, icons } from '@/constants'
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+     <ImageBackground
+        source={images.tatra1}
+        resizeMode="cover"
+        className='flex-1'
+      >
+        <View className='flex-1 justify-center items-center bg-black/50'>
+          <Text className='text-3xl text-white text-semibold font-psemibold text-center'>Tab available soon</Text>
+          <Image
+            source={icons.settings}
+            resizeMode='contain'
+            className='w-[200px] h-[200px]'
+          />
+        </View>
+      </ImageBackground>
   )
 }
 
