91bccc60bcd2fda72ee747fb0b7e9f17f5647859
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-01-13 20:12:13 +0100

    add adjustCameraToTrail

--- app/services/cameraService.ts
+++ app/services/cameraService.ts
@@ -9,3 +9,20 @@ export const centerCameraOnPeak = (item: any, camera: React.RefObject<Camera>): 
     zoomLevel: 15
   });
 }
+
+export const adjustCameraToTrail = (origin: any, destination: any, camera: React.RefObject<Camera>): void => {
+  const bounds: number[] = [
+    Math.min(origin[0], destination[0]),
+    Math.min(origin[1], origin[1]),
+    Math.max(origin[0], destination[0]),
+    Math.max(origin[1], origin[1]),
+  ]
+
+  camera.current?.setCamera({
+    centerCoordinate: [
+      (bounds[0] + bounds[2]) / 2,
+      (bounds[1] + bounds[3]) / 2,
+    ],
+    zoomLevel: 12,
+  });
+}
