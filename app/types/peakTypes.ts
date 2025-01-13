1ee792bac4d02d938e4483aad80cf251096798a9
Jakub Magiera <jakubmagiera.it@gmail.com>
2025-01-13 20:11:55 +0100

    separation of peak properties

--- app/types/peakTypes.ts
+++ app/types/peakTypes.ts
@@ -1,19 +1,21 @@
 export interface Peak {
   type: string;
-  properties: {
-    id: string;
-    ele: number | null;
-    "ele:bpv": string | null;
-    name: string | null;
-    "name:pl": string | null;
-    "name:sk": string | null;
-    natural: string | null;
-    wikipedia: string | null;
-    closest_trail_lon: number;
-    closest_trail_lat: number;
-  };
+  properties: PeakProperties;
   geometry: {
     type: string;
     coordinates: number[];
   };
+}
+
+export interface PeakProperties {
+  id: string;
+  ele: number | null;
+  "ele:bpv": string | null;
+  name: string | null;
+  "name:pl": string | null;
+  "name:sk": string | null;
+  natural: string | null;
+  wikipedia: string | null;
+  closest_trail_lon: number;
+  closest_trail_lat: number;
 }
